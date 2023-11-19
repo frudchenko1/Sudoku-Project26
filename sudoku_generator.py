a9fa2d911209211aaf8df6610c7261e41a229d80
KarRaj21 <149024586+KarRaj21@users.noreply.github.com>
2023-11-19 17:30:22 -0500

    Update sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -91,7 +91,12 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def valid_in_box(self, row_start, col_start, num):
-        pass
+         for a in range (row_start,row_start+3):
+        for b in range (col_start, col_start+3):
+          if num == Board[a][b]:
+            return False
+      return True
+
     
     '''
     Determines if it is valid to enter num at (row, col) in the board
