34a8eb71e1cbb089c580edc22385df3d7c4601f9
gus192 <95265581+gus192@users.noreply.github.com>
2023-11-19 17:51:08 -0500

    Update sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -122,7 +122,15 @@ class SudokuGenerator:
 	Return: None
     '''
     def fill_box(self, row_start, col_start):
-        pass
+        board = self.board
+        for i in range(row_start, row_start + 3):
+            for b in range(col_start, col_start + 3):
+                while True:
+                    digit = random.randint(1, 9)
+                    if digit not in board and self.valid_in_box(row_start, col_start, digit):
+                        self.board[i][b] = digit
+                        board += digit
+        
     
     '''
     Fills the three boxes along the main diagonal of the board
