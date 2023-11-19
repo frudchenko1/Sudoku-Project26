48b2d13de05bd510bb22f3845f0f3394c85a71f1
gus192 <95265581+gus192@users.noreply.github.com>
2023-11-19 16:47:10 -0500

    Update sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -25,6 +25,9 @@ class SudokuGenerator:
     def __init__(self, row_length, removed_cells):
         self.row_length = row_length
         self.removed_cells = removed_cells
+	self.board = []
+        self.box_length = math.sqrt(row_length)
+
 
     def get_board(self):
 
